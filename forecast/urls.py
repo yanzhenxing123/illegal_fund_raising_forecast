df09d90053152a8097d40283442f6fbfe7a4b9a5
yanzhenxing123 <209243446@qq.com>
2021-08-12 23:41:27 +0800

    password

--- forecast/urls.py
+++ forecast/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('train/upload/', views.TrainUploadView.as_view(), name="train_upload"),
     path('train/download/', views.TrainDownloadView.as_view(), name="train_download"),
     path('result/', views.ResultView.as_view(), name="result"),
-    path('train/start/', views.TrainStartView.as_view(), name="train_start")
+    path('train/start/', views.TrainStartView.as_view(), name="train_start"),
     path('console/', ConsoleView.as_view(), "console"),
 ]
